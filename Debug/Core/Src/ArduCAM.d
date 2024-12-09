d3a72589be68535efc1617efed0de42681bdfc07
Lazza <nicola.lazzarin89@gmail.com>
2024-12-09 12:31:15 +0100

    initial commit

---
+++ Debug/Core/Src/ArduCAM.d
@@ -0,0 +1,5 @@
+Core/Src/ArduCAM.o: ../Core/Src/ArduCAM.c ../Core/Inc/ArduCAM.h \
+ ../Core/Inc/ov5642_regs.h ../Core/Inc/ArduCAM.h
+../Core/Inc/ArduCAM.h:
+../Core/Inc/ov5642_regs.h:
+../Core/Inc/ArduCAM.h:
